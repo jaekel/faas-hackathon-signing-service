623e2220ba4e3eda612a3f8eac3235bc8562ad06
Andreas Jaekel <andreas.jaekel@ionos.com>
2022-06-23 10:13:30 +0200

    First skeleton draft of our signing function file.
    This version verifies the input (hmac) and returns a dummy
    output record, but it doesn't actually sign anything, yet.

---
+++ Sign.js
@@ -0,0 +1,67 @@
+/* Sign.js
+ *
+ * Implementation of hmac signing as a FaaS service on the cm4all
+ * FaaS platform.
+ *
+ * This code requires the presence of a secret key for signing
+ * in the same directory. The filenam should be "private_key.pem".
+ */
+
+/* Input:
+ *   {
+ *     "hmac": "<sha256 hash-based message authentication code>"
+ *   }
+ *
+ * Output:
+ *   {
+ *     "hmac": "<sha256 hash-based message authentication code>"
+ *     "date": "2022-06-23T10:21:43.806Z"
+ *     "username": "sign.me@ionos.com",
+ *     "signature": "<base64 encoded signature record>"
+ *   }
+ */
+
+/* hard-coded username for now */
+const userName = "sign.me@ionos.com"
+
+function validateHMAC(hmac) {
+	if (hmac == null) {
+			return "HMAC parameter is missing.";
+	}
+    const regexExp = /^[a-f0-9]{64}$/gi;
+    if (!regexExp.test(hmac)) {
+        return 'Invalid input string';
+    }
+    return null;
+}
+
+function generatePayload(hmac, isoDate) {
+    const payload = hmac + '|' + isoDate + '|' + userName;
+    return payload;
+}
+
+exports.handler = async function(event, context, callback) {
+	/* verify input parameters */
+    const hmac = event.hmac;
+    const result = validateHMAC(hmac);
+    if (result !== null) {
+        return result;
+    }
+
+	/* generate signature record */
+    const isoDate = new Date().toISOString();
+    const payload = generatePayload(hmac);
+
+	/* sign the record */
+	// XXX TODO
+
+	/* Return response */
+	return {
+		"hmac": hmac,
+		"date": isoDate,
+		"username": userName,
+		"signature": "<TBD>",
+		"payload": payload
+	};
+};
+
