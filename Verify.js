7b0467729abe3e86d808f167e6f101496ecb3e48
Andreas Jaekel <andreas.jaekel@ionos.com>
2022-06-23 12:32:32 +0200

    first skelleton draft of Verify.js

---
+++ Verify.js
@@ -0,0 +1,70 @@
+/* Verify.js
+ *
+ * Implementation of signature record validation as a FaaS service on the
+ * cm4all FaaS platform.
+ *
+ * This code requires the presence of a public key for signing
+ * in the same directory. The filenam should be "public_key.pem".
+ */
+
+/* Input:
+ *   {
+ *     "signature": "<base64 encoded signature record>"
+ *   }
+ *
+ * Output:
+ *   {
+ *	   "signatureValid": true,
+ *	   "SignaturePayload":
+ *	   {
+ *       "hmac": "<sha256 hash-based message authentication code>"
+ *       "date": "2022-06-23T10:21:43.806Z"
+ *       "username": "sign.me@ionos.com",
+ *     }
+ *   }
+ */
+
+/* import required modules */
+const fs = require('fs');
+const crypto = require('crypto');
+
+/* hard-coded username for now */
+const userName = 'sign.me@ionos.com';
+
+function validateSignatureRecord(signature) {
+    if (signature == null) {
+        return 'Signature parameter is missing.';
+    }
+    const regexExp = /^[a-f0-9=]*$/gi;
+    if (!regexExp.test(signature)) {
+        return 'Invalid input string';
+    }
+    return null;
+}
+
+exports.handler = async function (event, context, callback) {
+    /* verify input parameters */
+    const signature = event.signature;
+    const result = validateSignature(signature);
+    if (result !== null) {
+        return result;
+    }
+
+    /* decode signature from Base64 to Buffer */
+
+	/* decrypt buffer using public key */
+
+	/* decoding failed? -> return error and exit */
+
+	/* split payload into hmac, date and username */
+
+    /* Return response */
+    return {
+		singatureValid: true,
+		{
+        	hmac: hmac,
+        	date: isoDate,
+        	username: userName
+		}
+    };
+};
